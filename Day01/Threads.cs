3c274eb988d3039ef6126bd6c70397a43eb827da
pkelava <68123613+pkelava@users.noreply.github.com>
2020-07-13 18:43:45 +0200

    Članci na Internet forumima

--- Day01/Threads.cs
+++ Day01/Threads.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,17 +8,18 @@ namespace ForumLibrary
     {
         static public List<Threads> AllThreads = new List<Threads>();
 
-        public string Title { get; }
+        public string Title { get; set; }
         public List<Comments> listComments = new List<Comments>();
-        public string PostCreator { get; private set; }
-        public DateTime TimeOfCreation { get; private set; }
-        public string Context { get; private set; }
+        public string PostCreator { get;  set; }
+        public DateTime TimeOfCreation { get; set; }
+        public string Context { get; set; }
 
         public Threads(User creator, string title, string context)
         {
             PostCreator = creator.UserName;
             Context = context;
             Title = title;
+            TimeOfCreation = DateTime.Now;
             AllThreads.Add(this);
         }        
 
