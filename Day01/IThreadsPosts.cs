9d7c04837fcaf1736c482bf301365e38daeccb13
pkelava <68123613+pkelava@users.noreply.github.com>
2020-07-13 18:45:50 +0200

    Interface za članke

--- Day01/IThreadsPosts.cs
+++ Day01/IThreadsPosts.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +6,7 @@ namespace ForumLibrary
 {
     public interface IThreadsPosts : IPosts
     {
+        string Title { get; set; }
         void PrintComments();
     }
 }
